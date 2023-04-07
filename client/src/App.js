4c83d9725d4a5c1b987fb31627ec74d3038e8d89
martinivnv <martinivnv2002@gmail.com>
2023-04-07 13:49:07 -0400

    Added main page with map

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
+import MainPage from "./components/main/MainPage";
 
 function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
-				<Register />
+				<MainPage />
 			</BrowserRouter>
 		</div>
 	);
---
+++ client/src/components/main/MainPage.js
@@ -0,0 +1,12 @@
+import Map from "./Map";
+import PageTemplate from "../pageTemplate/PageTemplate";
+
+const MainPage = () => {
+	return (
+		<PageTemplate>
+			<Map />
+		</PageTemplate>
+	);
+};
+
+export default MainPage;
