aceb1e64e03932cd2ba5395267cbe629491f3cd3
martinivnv <martinivnv2002@gmail.com>
2023-04-08 13:36:04 -0400

    Add ability to accept and decline offers

--- client/src/components/carpool/AvailableCarpool.js
+++ client/src/components/carpool/AvailableCarpool.js
@@ -1,24 +1,48 @@
-const AvailableCarpool = ({ destination, user, minutesAway }) => {
+import { useState } from "react";
+
+const AvailableCarpool = ({
+	destination,
+	user,
+	minutesAway,
+	onOfferAccepted,
+}) => {
+	const onAccept = (e) => {
+		window.alert(
+			`You have accepted ${user}'s carpool offer. They will pick you up in ${minutesAway} minutes.`
+		);
+		onOfferAccepted();
+	};
+
+	const [display, setDisplay] = useState(true);
+
 	return (
-		<div
-			style={{
-				margin: "1em",
-				padding: "0.5em 1em 1em 1em",
-				borderRadius: "0.5em",
-				backgroundColor: "#8c8c8c5e",
-			}}
-		>
-			<div>
-				<p>
-					{user} is{" "}
-					<span style={{ fontWeight: "bold" }}>{minutesAway} minutes</span> away
-				</p>
+		display && (
+			<div
+				style={{
+					margin: "1em",
+					padding: "0.5em 1em 1em 1em",
+					borderRadius: "0.5em",
+					backgroundColor: "#8c8c8c5e",
+				}}
+			>
 				<div>
-					<button>✅ Accept</button>
-					<button style={{ marginLeft: "0.4em" }}>❌ Reject</button>
+					<p>
+						{user} is{" "}
+						<span style={{ fontWeight: "bold" }}>{minutesAway} minutes</span>{" "}
+						away
+					</p>
+					<div>
+						<button onClick={onAccept}>✅ Accept</button>
+						<button
+							style={{ marginLeft: "0.4em" }}
+							onClick={() => setDisplay(false)}
+						>
+							❌ Reject
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>
+		)
 	);
 };
 
